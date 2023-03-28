0c6b879d21b8230dcb36da04864388afe67f5a10
projec1707 <pwroma@gmail.com>
2023-03-28 19:45:12 +0300

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Roman Riepa on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
